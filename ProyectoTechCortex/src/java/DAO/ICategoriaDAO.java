5c57687e08e18d17eb2694b7cf90240a85a586d4
YazminScs <113384144+YazminScs@users.noreply.github.com>
2024-11-06 09:33:39 -0500

    Borrando proyecto sin maven

--- ProyectoTechCortex/src/java/DAO/ICategoriaDAO.java
+++
@@ -1,8 +0,0 @@
-package DAO;
-
-import Modelo.Categoria;
-import java.util.List;
-
-public interface ICategoriaDAO {
-    List<Categoria> listarCategoria();
-}
--- ProyectoTechCortex/src/java/DAO/IMarcaDAO.java
+++
@@ -1,8 +0,0 @@
-package DAO;
-
-import Modelo.Marca;
-import java.util.List;
-
-public interface IMarcaDAO {
-    List<Marca> listarMarcas();
-}
